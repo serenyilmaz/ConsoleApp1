dde412f83f5c76990156429f2f68f36da4ce175f
Sony <Sony@DESKTOP-QP93MJM>
2024-11-28 17:16:34 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tek sayılar");
+            Console.WriteLine("-----------");
+            for (int i = 100; i <= 200; i += 1)
+            {
+                if (i % 2 == 1)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
